9bd61929c1990bf218402464a2c9d547f48709c5
brobeson <brobeson@users.noreply.github.com>
2025-11-18 21:13:48 -0700

    Finish Drawing a Triangle step 1: Base Code
    https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/00_Setup/00_Base_code.html

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Developer Guide
+
+Install dependencies according to the [Vulkan SDK documentation](https://docs.vulkan.org/tutorial/latest/02_Development_environment.html).
---
+++ source/triangle.cpp
@@ -0,0 +1,57 @@
+#define GLFW_INCLUDE_VULKAN
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+  #include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+constexpr uint32_t WIDTH{800};
+constexpr uint32_t HEIGHT{600};
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) { glfwPollEvents(); }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+
+  GLFWwindow* window{nullptr};
+};
+
+int main() {
+  HelloTriangleApplication app;
+
+  try {
+    app.run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
